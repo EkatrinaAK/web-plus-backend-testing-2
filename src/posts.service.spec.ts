b7530ee6ff7c29d0912027fe2160d5c4c56236f5
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-05-30 15:11:00 +0300

    Update posts.service.spec.ts

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,71 @@
-import { Post, PostsService } from './posts.service';
+import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
-  };
 
-  beforeEach(async () => {
+  beforeEach(() => {
     postsService = new PostsService();
-
-    postsService.create({ text: 'Some pre-existing post' });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
-  });
+  describe('.findMany', () => {
+    const posts = [
+      {text: 'Post 1'},
+      {text: 'Post 2'},
+      {text: 'Post 3'},
+      {text: 'Post 4'},
+    ];
+    const expectedPosts = [
+			{ id: "1", text: "Post 1" },
+			{ id: "2", text: "Post 2" },
+			{ id: "3", text: "Post 3" },
+			{ id: "4", text: "Post 4" },
+		];
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+    beforeEach(() => {
+      posts.forEach((post) => postsService.create(post));
+    });
+
+    it('should return all posts if called without options', () => {
+      // реализуйте тест-кейс
+      const foundPosts = postsService.findMany();
+			expect(foundPosts).toEqual(expectedPosts);
+    });
+
+    it('should return correct posts for skip and limit options', () => {
+      // реализуйте тест-кейс
+      const limitedPosts = postsService.findMany({ limit: 2 });
+			const expectedResultLimited = expectedPosts.slice(0, 2);
+			expect(limitedPosts).toEqual(expectedResultLimited);
+
+			const skippedPosts = postsService.findMany({ skip: 2 });
+			const expectedResultSkipped = expectedPosts.slice(2);
+			expect(skippedPosts).toEqual(expectedResultSkipped);
+
+			const skippedAndLimitedPosts = postsService.findMany({
+				skip: 1,
+				limit: 2,
+			});
+			const expectedResultSkippedAndLimited = expectedPosts.slice(1, 3);
+			expect(skippedAndLimitedPosts).toEqual(expectedResultSkippedAndLimited);
+    });
+
+    // реализуйте недостающие тест-кейсы
+    it("should return empty array if limit is not a number or a zero", () => {
+			const limitedByNaNPosts = postsService.findMany({ limit: NaN });
+			expect(limitedByNaNPosts).toEqual([]);
+
+			const limitedByZeroPosts = postsService.findMany({ limit: 0 });
+			expect(limitedByZeroPosts).toEqual([]);
+		});
+
+		it("should return an empty array if skip is greater than the number of posts", () => {
+			const result = postsService.findMany({ skip: 10 });
+			expect(result).toEqual([]);
+		});
+
+		it("should return an empty array if skip and limit are both greater than the number of posts", () => {
+			const result = postsService.findMany({ skip: 5, limit: 5 });
+			expect(result).toEqual([]);
+		});
   });
 });
